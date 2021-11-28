4c1966f7e160dc294c944dcad2f6f5928ac049f3
Daniel Melo <melo.daniel00@gmail.com>
2021-11-28 15:50:56 -0300

    PHP-UNIT - Concluído capítulo 2 do curso

--- src/Service/Avaliador.php
+++ src/Service/Avaliador.php
@@ -5,23 +5,29 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private $maiorValor;
+    private $maiorValor = -INF;
+    private $menorValor = INF;
 
-    /**
-     * @param Leilao $leilao
-     */
     public function avalia(Leilao $leilao): void
     {
-        $lances = $leilao->getLances();
-        $ultimoLance = $lances[count($lances) - 1];
-        $this->maiorValor = $ultimoLance->getValor();
+        foreach ($leilao->getLances() as $lance) {
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
+        }
     }
 
-    /**
-     * @return mixed
-     */
     public function getMaiorValor(): float
     {
         return $this->maiorValor;
+    }
+
+    public function getMenorValor(): float
+    {
+        return $this->menorValor;
     }
 }
