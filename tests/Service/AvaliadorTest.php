5b96faa6b87725720710444c8b0f0cd783db8a67
Daniel Melo <melo.daniel00@gmail.com>
2021-11-28 18:34:14 -0300

    PHP-UNIT - Concluído terceiro capítulo

--- tests/Service/AvaliadorTest.php
+++ tests/Service/AvaliadorTest.php
@@ -60,7 +60,7 @@ class AvaliadorTest extends TestCase
     }
     public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemDecrescente()
     {
-        // Arrange - Given
+        # Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
 
         $maria = new Usuario('Maria');
@@ -71,18 +71,18 @@ class AvaliadorTest extends TestCase
 
         $leiloeiro = new Avaliador();
 
-        // Act - When
+        # Act - When
         $leiloeiro->avalia($leilao);
 
         $menorValor = $leiloeiro->getMenorValor();
 
-        // Assert - Then
+        # Assert - Then
         self::assertEquals(2000, $menorValor);
     }
 
     public function testAvaliadorDeveEncontrarOMenorValorDeLancesEmOrdemCrescente()
     {
-        // Arrange - Given
+        # Arrange - Given
         $leilao = new Leilao('Fiat 147 0KM');
 
         $maria = new Usuario('Maria');
@@ -93,13 +93,35 @@ class AvaliadorTest extends TestCase
 
         $leiloeiro = new Avaliador();
 
-        // Act - When
+        # Act - When
         $leiloeiro->avalia($leilao);
 
         $menorValor = $leiloeiro->getMenorValor();
 
-        // Assert - Then
+        # Assert - Then
         self::assertEquals(2000, $menorValor);
     }
 
+    public function testAvaliadorDeveBuscar3MaioresValores()
+    {
+        $leilao = new Leilao('Fiat 147 0KM');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+        static::assertCount(3, $maiores);
+        static::assertEquals(2000, $maiores[0]->getValor());
+        static::assertEquals(1700, $maiores[1]->getValor());
+        static::assertEquals(1500, $maiores[2]->getValor());
+    }
 }
