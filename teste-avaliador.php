a495716bc294b33fa5815387678d38217d059b2e
Daniel Melo <melo.daniel00@gmail.com>
2021-11-27 21:16:10 -0300

    PHP-UNIT - Concluído primeiro capítulo do curso

--- teste-avaliador.php
+++ teste-avaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+# Arrange - Given
 $leilao = new Leilao('Fiat 147 0 Km');
 $daniel = new Usuario('Daniel');
 $duda = new Usuario('Duda');
@@ -15,3 +16,19 @@ $leilao->recebeLance(new Lance($daniel, 4580));
 $leilao->recebeLance(new Lance($duda, 4895));
 
 $leiloeiro = new Avaliador();
+
+# Act - When
+$leiloeiro->avalia($leilao);
+
+$maiorValor = $leiloeiro->getMaiorValor();
+
+# Assert - Then
+$valorEsperado = 4895;
+
+if ($valorEsperado == $maiorValor) {
+    echo 'Passou no teste';
+
+    return;
+}
+
+echo 'Teste falhou com êxito';
